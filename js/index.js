0c1a87dd75e08aa7776bb963406d043970c2dd39
SCaucota <masocaucota@gmail.com>
2024-05-26 15:14:38 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,132 +1,37 @@
-const divNombre = document.getElementById("divNombre");
-const divTelefono = document.getElementById("divTelefono");
-const divSelectProvincia = document.getElementById("divSelectProvincia");
-const divEmail = document.getElementById("divEmail");
-const divAsunto = document.getElementById("divAsunto");
-const divMensaje = document.getElementById("divMensaje");
-const divCheckbox = document.getElementById("divCheckbox");
+const leftButton = document.querySelector(".prev");
+const rightButton = document.querySelector(".next");
+const cardWrapper = document.querySelector(".carouselWrapper");
+const cards = document.querySelectorAll(".carouselCard");
+const itemsToShow = 3;
+let currentIndex = 0;
+const totalItems = cards.length;
 
-const nombreInput = document.getElementById("nombre");
-const telefonoInput = document.getElementById("telefono");
-const selectProvincia = document.getElementById("selectProvincia");
-const mailInput = document.getElementById("mail");
-const asuntoInput = document.getElementById("asunto");
-const mensajeInput = document.getElementById("inputMensaje");
-const checkboxInput = document.getElementById("politicaPrivacidad");
-
-const nombreError = document.createElement("p");
-const telefonoError = document.createElement("p");
-const selectProvinciaError = document.createElement("p");
-const mailError = document.createElement("p");
-const asuntoError = document.createElement("p");
-const mensajeError = document.createElement("p");
-const checkboxError = document.createElement("p");
-
-const palabraExpresion = /^[A-Za-z\s'-]+$/;
-const emailExpresion = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-const numerosExpresion = /^(0|[1-9]\d*)(\.\d+)?$/;
-
-function validation(event) {
-
-    event.preventDefault();
-
-    let isValid = true;
-
-    if(nombreInput.value === "") {
-        isValid = false;
-        nombreError.textContent = "Campo incompleto";
-        nombreError.style.color = "red";
-        divNombre.appendChild(nombreError);
-    }else if(!palabraExpresion.test(nombreInput.value)){
-        isValid = false;
-        nombreError.textContent = "Nombre inválido";
-        nombreError.style.color = "red";
-        divNombre.appendChild(nombreError);
-    }else {
-        nombreError.remove();
+rightButton.addEventListener('click', () => {
+    if (currentIndex < totalItems - itemsToShow) {
+        currentIndex += itemsToShow;
+        updateCarousel();
     }
+});
 
-    if(telefonoInput.value === "") {
-        isValid = false;
-        telefonoError.textContent = "Campo incompleto";
-        telefonoError.style.color = "red";
-        divTelefono.appendChild(telefonoError);
-    }else if(!numerosExpresion.test(telefonoInput.value)){
-        isValid = false;
-        telefonoError.textContent = "Teléfono inválido";
-        telefonoError.style.color = "red";
-        divTelefono.appendChild(telefonoError);
-    }else {
-        telefonoError.remove();
+leftButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex -= itemsToShow;
+        updateCarousel();
     }
+});
 
-    if(selectProvincia.value === "0") {
-        isValid = false;
-        selectProvinciaError.textContent = "Elige una opción";
-        selectProvinciaError.style.color = "red";
-        divSelectProvincia.appendChild(selectProvinciaError);
-    }else {
-        selectProvinciaError.remove();
-    }
-
-    if(mailInput.value === "") {
-        isValid = false;
-        mailError.textContent = "Campo incompleto";
-        mailError.style.color = "red";
-        divEmail.appendChild(mailError);
-    }else if(!emailExpresion.test(mailInput.value)) {
-        isValid = false;
-        mailError.textContent = "Mail inválido";
-        mailError.style.color = "red";
-        divEmail.appendChild(mailError);
-    }else {
-        mailError.remove();
-    }
-
-    if(asuntoInput.value === "") {
-        isValid = false;
-        asuntoError.textContent = "Campo incompleto";
-        asuntoError.style.color = "red";
-        divAsunto.appendChild(asuntoError);
-    }else {
-        asuntoError.remove();
-    }
-
-    if(mensajeInput.value === "") {
-        isValid = false;
-        mensajeError.textContent = "Campo incompleto";
-        mensajeError.style.color = "red";
-        divMensaje.appendChild(mensajeError);
-    }else {
-        mensajeError.remove();
-    }
-
-    if(!checkboxInput.checked) {
-        isValid =  false;
-        checkboxError.textContent = "Debes aceptar las politicas de privacidad";
-        checkboxError.style.color = "red";
-        divCheckbox.insertAdjacentElement("afterend", checkboxError);;
-    }else {
-        checkboxError.remove();
-    }
-
-    if(isValid) {
-        mensajeEnviado();
-    }
+function updateCarousel() {
+    const cardContainer = document.querySelector('.cardContainer');
+    const itemWidth = document.querySelector('.carouselCard').offsetWidth;
+    const newTransformValue = -(itemWidth * currentIndex);
+    cardContainer.style.transform = `translateX(${newTransformValue}px)`;
+    checkButtons();
 }
 
-const divForm = document.getElementById("formContacto");
-const textoInputs = document.querySelector(".textoInputsContacto")
-
-function mensajeEnviado() {
-    textoInputs.innerHTML = "";
-    divForm.innerHTML = "";
-    divForm.innerHTML = `
-        <h3>¡Genial, ${nombreInput.value}!</h3>
-        <h4>Tu mensaje se ha enviado exitosamente</h4>
-    `
+function checkButtons() {
+    leftButton.disabled = currentIndex === 0;
+    rightButton.disabled = currentIndex >= totalItems - itemsToShow;
 }
 
-const botonEnviar = document.querySelector("#buttonEnviar");
+checkButtons();
 
-botonEnviar.addEventListener("click", validation);
--- js/tienda.js
+++ js/tienda.js
@@ -1,20 +1,20 @@
 const seccionTienda = document.getElementById("sectionTienda");
 
 const libros = [
-    {titulo: "Bajo la misma estrella", categoria: "romance",precio: 8999, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735724/bajo_la_misma_estrella_kvks2g.jpg"},
-    {titulo: "Eleanor y Park", categoria: "romance",precio: 12500, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735740/eleanor_y_park_cbhbxr.jpg"},
-    {titulo: "El hobbit", categoria: "fantasia",precio: 11000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735740/el_hobbit_amawdi.jpg"},
-    {titulo: "Cinder", categoria: "ciencia ficcion",precio: 12300, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735740/cinder_hdarb7.jpg"},
-    {titulo: "Escrito en el agua", categoria: "misterio",precio: 15000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735752/escrito_en_el_agua_mkdma9.jpg"},
-    {titulo: "La chica del tren", categoria: "misterio",precio: 14200, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/La_Chica_Del_Tren_uoafps.jpg"},
-    {titulo: "Harry Potter y la piedra filosofal", categoria: "fantasia",precio: 12600, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/Harry_Potter_y_la_piedra_filosofal_rjsysk.jpg"},
-    {titulo: "La quinta ola", categoria: "ciencia ficcion",precio: 9600, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/la_quinta_ola_ay0jji.jpg"},
-    {titulo: "Si decido quedarme", categoria: "romance",precio: 12100, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/Si_decido_quedarme_eiaz55.jpg"},
-    {titulo: "Los juegos del hambre", categoria: "ciencia ficcion",precio: 17000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/los_juegos_del_hambre_k6ljkx.jpg"},
-    {titulo: "Yo antes de ti", categoria: "romance",precio: 12000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/Yo_Antes_De_Ti_gi65pm.jpg"},
-    {titulo: "¿Quién mató a Alex?", categoria: "misterio",precio: 14000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/quien_mato_a_alex_wzwu9m.jpg"},
-    {titulo: "La selección", categoria: "reomance",precio: 7000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735773/la_seleccion_whjbtm.jpg"},
-    {titulo: "Correr o Morir", categoria: "ciencia ficcion",precio: 16000, img: "https://res.cloudinary.com/dcwuqrvuv/image/upload/v1715735774/maze_runner_jwntht.jpg"}
+    {titulo: "Bajo la misma estrella", categoria: "romance",precio: 8999, img: "../img/bajo-la-misma-estrella.jpg"},
+    {titulo: "Eleanor y Park", categoria: "romance",precio: 12500, img: "../img/eleanor-y-park.jpg"},
+    {titulo: "El hobbit", categoria: "fantasia",precio: 11000, img: "../img/el-hobbit.jpg"},
+    {titulo: "Los pilares de la tierra", categoria: "fantasia",precio: 12300, img: "../img/los-pilares-de-la-tierra.jpg"},
+    {titulo: "Escrito en el agua", categoria: "misterio",precio: 15000, img: "../img/escrito-en-el-agua.jpg"},
+    {titulo: "La chica del tren", categoria: "misterio",precio: 14200, img: "../img/la-chica-del-tren.jpg"},
+    {titulo: "Harry Potter y la piedra filosofal", categoria: "fantasia",precio: 12600, img: "../img/harry-potter-y-la-piedra-filosofal.jpg"},
+    {titulo: "Juego de tronos: Canción de fuego y hielo", categoria: "fantasia",precio: 9600, img: "../img/juego-de-tronos-cancion-de-hielo-y-fuego.jpg"},
+    {titulo: "Tiempo de matar", categoria: "terror",precio: 12100, img: "../img/tiempo-de-matar.jpg"},
+    {titulo: "Los juegos del hambre", categoria: "ciencia ficcion",precio: 17000, img: "../img/los-juegos-del-hambre.jpg"},
+    {titulo: "Mil soles espléndidos", categoria: "novela",precio: 50000, img: "../img/mil-soles-esplendidos.jpg"},
+    {titulo: "¿Quién mató a Alex?", categoria: "misterio",precio: 14000, img: "../img/quien-mato-a-alex.jpg"},
+    {titulo: "La selección", categoria: "reomance",precio: 7000, img: "../img/la-seleccion.jpg"},
+    {titulo: "22/11/63", categoria: "terror",precio: 9000, img: "../img/22-11-63.jpg"}
 ]
 
 const divContainerLibros = document.createElement("div");
@@ -56,7 +56,7 @@ function crearCardLibro(libro) {
 
     const precio = document.createElement("h5");
     precio.classList.add("precioCard");
-    precio.textContent = `$${libro.precio}`;
+    precio.textContent = `$${libro.precio.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
 
     const botonAgregarCarrito = document.createElement("button");
     botonAgregarCarrito.classList.add("boton");
